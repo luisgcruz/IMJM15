1192a50f3810c2a82dd0fdf9ebfc5928d695d310
unknown <fmanime@gmail.com>
2022-06-17 17:18:48 -0500

    [ADD] Se agregan los módulos custom ya corregidos

--- account_invoice_imj/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models, api,_
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    es_vendedor = fields.Boolean(string="¿Es vendedor?", default=False)
--- budget_purchase_imj/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import account
-from . import purchase
-
