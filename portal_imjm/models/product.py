d8cdda2a1881c6f5d3241f03a201983f41837a18
unknown <fmanime@gmail.com>
2022-06-17 19:00:37 -0500

    [REM] Se eliminaron los módulos no actualizados

--- portal_imjm/models/product.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class ProductSatCode(models.Model):
-    _inherit = 'l10n_mx_edi.product.sat.code'
-
-    aceptable = fields.Boolean(help='Si no es aceptable, rechazará el intento de crear la factura desde el portal.', default=False)
